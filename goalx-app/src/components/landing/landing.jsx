e1a4968c7a2442b951a4e1603afe86f82f368e2d
SWMakeAnImpact <wilkessarah96@gmail.com>
2023-12-06 13:57:04 -0500

    Your commit message

---
+++ goalx-app/src/components/landing/landing.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+import "./landing.css";
+
+export const Landing = () => {
+    return (
+        <div className="landing">
+            <div className="div">
+                <div className="overlap">
+                    <div className="overlap-group">
+                        <div className="group">
+                            <div className="text">{""}</div>
+                            <div className="text-wrapper">About us</div>
+                            <p className="support">
+                                <span className="span">
+                                    Support
+                                    <br />
+                                </span>
+                                <span className="text-wrapper-2">
+                                    <br />
+                                </span>
+                            </p>
+                            <div className="text-wrapper-3">Terms of Service</div>
+                            <div className="text-wrapper-4">Privacy Policy</div>
+                            <div className="rectangle" />
+                        </div>
+                        <div className="div-wrapper">
+                            <div className="text-wrapper-5">Connect with us</div>
+                        </div>
+                        <img className="frame" alt="Frame" src="frame-2.svg" />
+                    </div>
+                    <img className="goalx" alt="Goalx" src="goalx-01-1.png" />
+                </div>
+                <div className="overlap-2">
+                    <img className="young-student" alt="Young student" src="young-student-working-assignment-1.png" />
+                    <img className="img" alt="Group" src="group-3.png" />
+                    <div className="group-2">
+                        <div className="text-wrapper-6">Log in</div>
+                    </div>
+                    <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                            <div className="text-wrapper-7">Sign up</div>
+                        </div>
+                    </div>
+                    <div className="group-3">
+                        <p className="p">Advance Your Career with GoalX</p>
+                    </div>
+                    <div className="group-4">
+                        <p className="text-wrapper-8">Where Next is a Click Away</p>
+                    </div>
+                    <img className="group-5" alt="Group" src="group.png" />
+                    <div className="group-6">
+                        <div className="text-wrapper-9">Get started</div>
+                        <img className="vector" alt="Vector" src="image.svg" />
+                    </div>
+                    <div className="rectangle-2" />
+                    <img className="goalx-2" alt="Goalx" src="goalx-01-1-2.png" />
+                    <div className="group-7">
+                        <div className="text-wrapper-6">Log in</div>
+                    </div>
+                    <div className="overlap-wrapper">
+                        <div className="overlap-3">
+                            <div className="text-wrapper-7">Sign up</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="group-wrapper">
+                    <div className="group-8">
+                        <div className="text-wrapper-10">Empowering Your Potiental</div>
+                    </div>
+                </div>
+                <div className="overlap-4">
+                    <div className="group-9">
+                        <div className="text-wrapper-11">Join now</div>
+                    </div>
+                </div>
+                <p className="text-wrapper-12">Hear success stories directly from our users.</p>
+                <p className="text-wrapper-13">Join the GoalX community and start building your future today</p>
+                <img className="vector-2" alt="Vector" src="vector-3.svg" />
+                <p className="text-wrapper-14">Explore Your Potential with GoalX</p>
+                <div className="icon-question">
+                    <div className="group-10">
+                        <div className="overlap-5">
+                            <div className="group-11">
+                                <div className="group-12">
+                                    <div className="overlap-group-3">
+                                        <div className="group-13">
+                                            <p className="text-wrapper-15">
+                                                Customized career paths and opportunities based on your interests and goals.
+                                            </p>
+                                        </div>
+                                        <div className="text-wrapper-16">Personalized Recommendations</div>
+                                    </div>
+                                </div>
+                                <div className="group-14">
+                                    <div className="overlap-group-3">
+                                        <div className="group-13">
+                                            <p className="text-wrapper-15">
+                                                Discover your potential and preferences with our engaging career quiz
+                                            </p>
+                                        </div>
+                                        <div className="text-wrapper-17">Interactive Career Quiz</div>
+                                    </div>
+                                </div>
+                                <div className="group-15">
+                                    <div className="overlap-group-3">
+                                        <div className="group-13">
+                                            <p className="text-wrapper-15">Connect with professionals and mentors in your field.</p>
+                                        </div>
+                                        <div className="text-wrapper-17">Networking Platform</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <img className="vector-3" alt="Vector" src="vector-4.svg" />
+                            <img className="icon-question-2" alt="Icon question" src="icon-question.png" />
+                            <img className="icon-self-healing" alt="Icon self healing" src="icon-self-healing.png" />
+                        </div>
+                    </div>
+                </div>
+                <div className="overlap-6">
+                    <div className="group-16">
+                        <div className="overlap-7">
+                            <div className="flexcontainer">
+                                <p className="span-wrapper">
+                                    <span className="text-wrapper-18">
+                                        Alex&#39;s Transformation with Golex
+                                        <br />
+                                    </span>
+                                </p>
+                                <p className="span-wrapper">
+                                    <span className="text-wrapper-19">
+                                        Alex, an aspiring software developer, faced challenges breaking into the tech industry due to a lack
+                                        of formal education in computer science. Determined to transition careers, Alex discovered Golex, an
+                                        online learning platform renowned for its comprehensive programming courses.
+                                    </span>
+                                </p>
+                            </div>
+                            <img className="mask-group" alt="Mask group" src="mask-group-2.png" />
+                        </div>
+                        <div className="flexcontainer-wrapper">
+                            <div className="flexcontainer-2">
+                                <p className="text-2">
+                                    <span className="text-wrapper-20">
+                                        Mia&#39;s Journey to Success with Golex
+                                        <br />
+                                    </span>
+                                </p>
+                                <p className="text-2">
+                                    <span className="text-wrapper-21">
+                                        Starting with the fundamentals, Mia immersed herself in Golex&#39;s comprehensive graphic design
+                                        curriculum. The platform&#39;s interactive lessons, curated by industry experts, provided Mia with
+                                        in-depth knowledge of design principles, software proficiency, and practical skills crucial for
+                                        success in the field.
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="overlap-8">
+                            <div className="flexcontainer-3">
+                                <p className="span-wrapper">
+                                    <span className="text-wrapper-18">
+                                        Alex&#39;s Transformation with Golex
+                                        <br />
+                                    </span>
+                                </p>
+                                <p className="span-wrapper">
+                                    <span className="text-wrapper-19">
+                                        Alex, an aspiring software developer, faced challenges breaking into the tech industry due to a lack
+                                        of formal education in computer science. Determined to transition careers, Alex discovered Golex, an
+                                        online learning platform renowned for its comprehensive programming courses.
+                                    </span>
+                                </p>
+                            </div>
+                            <img className="mask-group-2" alt="Mask group" src="mask-group.png" />
+                        </div>
+                    </div>
+                    <img className="mask-group-3" alt="Mask group" src="image.png" />
+                </div>
+                <div className="group-17">
+                    <div className="ellipse" />
+                    <div className="ellipse-2" />
+                    <div className="ellipse-3" />
+                </div>
+            </div>
+        </div>
+    );
+};
